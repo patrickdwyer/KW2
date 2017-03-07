5c12085f1890b98547d347ca77662ece2fa4c5e2
Patrick Dwyer <patrick@e-mediaresources.com>
2017-03-07 16:10:37 -0600

    Built “Case Study” template

---
+++ public_html/case-study.php
@@ -0,0 +1,210 @@
+<?php include '_includes/header.php';?>
+
+	<header class="header page-title case-study" style="background-image: url(/img/bg-goodstock.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2">
+					<p class="eyebrow">&nbsp;</p>
+					<h1 class="title">The Road Home</h1>
+					<p class="eyebrow">Goodstock goes to work for homeless children and their families</p>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<article class="base-blue">
+
+		<section class="bg-primary case-study-intro">
+			<div class="container content-container">
+				<div class="row">
+					<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+						<!--start content-->
+						<div class="row">
+							<div class="col-sm-8">
+								<h1 class="lead">It’ll happen tonight because it happens almost every night. Whole families will be turned away from homeless shelters that are too crowded to take them in. Luckily for many, The Road Home offers stable housing and support. KW2 asked how we could help.</h1>
+							</div>
+							<div class="col-sm-3 col-sm-offset-1">
+								<p class="eyebrow">What We Did</p>
+								<ul class="whatwedid">
+									<li>Marketing Strategy & Planning</li>
+									<li>Messaging Strategy</li>
+									<li>Television Ad</li>
+									<li>Out of Home</li>
+									<li>Print Ads</li>
+								</ul>
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-base pt20 pb20">
+			<div class="container-fluid content-container content-media-2">
+				<div class="row">
+					<div class="col-sm-12">
+						<!--start content-->
+						<p class="text-center">Single Image Block</p>
+						<div class="row">
+							<div class="col-sm-12">
+								<img src="http://placehold.it/1200x388" alt="" class="img-responsive">
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-base pt0 pb0">
+			<div class="container-fluid content-container content-media-1">
+				<div class="row">
+					<div class="col-sm-12">
+						<!--start content-->
+						<p class="text-center">Video Block</p>
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe src="https://www.youtube.com/embed/OivMe-nJyDo?ecver=2&showinfo=0"></iframe>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-base">
+			<div class="container content-container">
+				<div class="row">
+					<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+						<!--start content-->
+						<div class="row">
+							<div class="col-sm-8">
+								<h1>It’s hard to fix a problem no one can see.</h1>
+								<p>In the Madison area alone, there are nearly 1000 homeless families. And in 2013, at least 400 Dane County families (including 775 children) couldn’t find room in a community shelter. The numbers are stunning—at least, they surprised our Goodstock team. But The Road Home staff understood. It seems homeless families aren’t that easy to see.</p>
+								<p>When the shelters are full, homeless families seek out quiet, hidden places to protect their kids from judgment and harm. They sleep in cars, hole up in empty buildings; you don’t often see families living on the streets. And that presented a challenge for The Road Home Dane County. To help an ever-increasing number of homeless children and their families, the organization needed to drive support for a cause that very few people are aware of.</p>
+								<p>KW2 had it’s mission. We needed to make an invisible problem visible in 24 hours, give or take.</p>
+							</div>
+							<div class="col-sm-4">
+								<div class="sidebar mt0">
+									<img src="http://placehold.it/177x177" alt="" class="photo-circle mb30 img-responsive">
+									<p class="factoid">Families make up 45% of Dane County’s homeless population.</p>
+								</div>
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-primary">
+			<div class="container content-container">
+				<div class="row">
+					<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+						<!--start content-->
+						<div class="row">
+							<div class="col-sm-8">
+								<h1>Creating a window into homelessness.</h1>
+								<p>The Road Home needed a marketing plan that could make the most of their limited budget and an advertising campaign capable of bringing the realities of childhood and family homelessness into focus. Isolation, uncertainty, shuffling endlessly from place to place—these poignant truths would speak for themselves. But to increase donations, we’d have to do more than make an emotional connection to the cause. We’d need to build brand recognition. We’d have to make The Road Home’s name the central idea of our television spot.</p>
+								<p>The clock was ticking. With Goodstock partner Nick Berard behind the lens and just a few hours of available light, our creative team searched out Madison’s hidden corners and abandoned spaces. Wisconsin’s dreary winter weather helped, adding a visible chill to each location.</p>
+								<div class="embed-responsive embed-responsive-16by9">
+									<iframe src="https://www.youtube.com/embed/OivMe-nJyDo?ecver=2&showinfo=0"></iframe>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="sidebar mt0">
+									<h1>New Sidebar</h1>
+									<p class="factoid">When your family is living on the streets, the road home changes every night.</p>
+								</div>
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-primary pt0 pb0">
+			<div class="container-fluid content-container content-media-2">
+				<div class="row">
+					<div class="col-sm-12">
+						<!--start content-->
+						<p class="text-center">2-Col Image Block</p>
+						<div class="row">
+							<div class="col-sm-6">
+								<img src="http://placehold.it/586x388" alt="" class="img-responsive">
+							</div>
+							<div class="col-sm-6">
+								<img src="http://placehold.it/586x388" alt="" class="img-responsive">
+							</div>
+							<div class="col-sm-6">
+								<img src="http://placehold.it/586x388" alt="" class="img-responsive">
+							</div>
+							<div class="col-sm-6">
+								<img src="http://placehold.it/586x388" alt="" class="img-responsive">
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-base">
+			<div class="container content-container">
+				<div class="row">
+					<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+						<!--start content-->
+						<div class="row">
+							<div class="col-sm-8">
+								<h1>Donors increased by 35%.</h1>
+								<p>With the help of generous partners, our Goodstock give included a strategic marketing plan, television spot, print ads and out-of-home creative for The Road Home, plus 450 minutes of free airtime and 4 free insertion in Madison’s biggest local publications.</p>
+								<p>It all worked. Boosting The Road Home’s visibility increased donors and donations in the year that followed. Their donor pool grew by 35%—a 12% jump in new donors over the previous year—and Individual contributions rose by $19,000 between 2014 and 2015.</p>
+								<img src="http://placehold.it/800x388" alt="" class=" mt20 mb20 img-responsive">
+								<p>But underneath the clean layout and easy-to-use tools, there’s a powerful information engine at work. Making sure every Wisconsin voter receives the personal, up-to-date information they need wasn’t easy. To ensure always-accurate results, KW2 worked with a team of talented development partners to build a robust database system capable of parsing a state’s worth of complex, secured data—including voter registration status, election dates, polling place locations, ballot designs and more.</p>
+							</div>
+							<div class="col-sm-4">
+								<div class="sidebar mt0">
+									<p class="eyebrow">Digitizing Our Modern World With Style.</p>
+									<p class="quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu hendrerit quam. "</p>
+									<img src="http://placehold.it/177x177" alt="" class="photo-circle mb30 img-responsive">
+									<h3 class="name">Tim Christian</h3>
+									<p class="eyebrow undercase">KW2's Digital Director</p>
+								</div>
+							</div>
+						</div>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="bg-primary testimonial-block">
+			<div class="container content-container">
+				<div class="row">
+					<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+						<!--start content-->
+						<p class="quote">“I have something wonderful to say about KW2.”</p>
+						<p class="eyebrow">Name A, VP of Name at Name Co</p>
+						<!--end content-->
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</article>
+
+	<section class="bg-primary next-project" style="background-image: url(/img/bg-f-a-c-t.jpg);">
+		<div class="container content-container">
+			<div class="row">
+				<div class="col-sm-12 col-lg-10 col-lg-offset-1">
+					<!--start content-->
+					<p class="eyebrow">Next Project</p>
+					<h2 class="title"><a href="#">MyVote Wisconsin</a></h2>
+					<!--end content-->
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php include '_includes/footer.php';?>
